25eef568e8c5e44f174c5dca0ab564dff730f876
cv4497 <4497cv@gmail.com>
2021-04-07 12:20:06 -0500

    state detected

--- teams_snyp.py
+++ teams_snyp.py
@@ -8,11 +8,6 @@ from pynput import mouse
 from pynput import keyboard
 import time
 import sys
-import string
-
-RGB_AWAY = (253,198,63)
-RGB_ONLINE = (146, 195, 83)
-RGB_BUSY = (196,49,75)
 
 def selectText(x,y):
     from pynput.mouse import Controller, Button
@@ -32,40 +27,26 @@ def locate_and_click(file_name, conf):
         selectText(x,y)
   
 
-def getTeamsState():
-    pixel = pyautogui.pixel(1347, 105)
-    if(RGB_AWAY == pixel):
-        return "AWAY"
-    elif(RGB_BUSY == pixel):
-        return "BUSY"
-    elif(RGB_ONLINE == pixel):
-        return "ONLINE"
+def check_teams_state(file_name, conf):
+    b = pyautogui.locateOnScreen(file_name, grayscale=True, confidence =conf)
+
+    if(b != None):
+        print("The user is currently online")
+        return "OFFLINE"
     else:
-        return "NOT_A_STATE"
-            
+        print("The user is currently offline")
+        return "ONLINE"
 
 
 def main():
-    from pynput.keyboard import Key, Controller
-    keyboard = Controller()
-    
-    locate_and_click('img/teams.png', 1)
-    
-    while(1):
-        state = getTeamsState()
-        print(state)
-            
-##        if(state == "AWAY"):
-##            locate_and_click('img/chat.png',.8)
-##            time.sleep(2)
-##            locate_and_click('img/teams.png',.8)
-        time.sleep(10)
-            
+    image = pyautogui.screenshot()
+    pix = pyautogui.pixel(1347, 105)
+    print(pix)
+    pyautogui.mouseInfo()
     sys.exit(0)
     
 main()
 
-#pyautogui.mouseInfo()
 ##image = pyautogui.screenshot()
 ##image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
 ##cv2.imwrite("in_memory_to_disk.png", image)
